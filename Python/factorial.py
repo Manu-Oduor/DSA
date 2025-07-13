a4afc7df83a3d0b76a8a1b62e7296dd73a37f8ad
Manu-Oduor <eooduor31@gmail.com>
2025-07-13 15:57:11 +0300

    initial commit

--- Python/factorial.py
+++ Python/factorial.py
@@ -7,3 +7,11 @@ def factorial (num):
 
 num =5
 print("the factorial for", num ,"is",factorial(5))
+
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(6))
